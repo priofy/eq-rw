58ad376dcbf16490d3164eb0579a08f6ce54f32a
0xjoohi <0xjoohi@gmail.com>
2023-12-07 21:56:13 +0400

    again env prod graphql

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,32 +1,15 @@
-module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
-    },
-  },
+module.exports = {
   graphql: {
-    enabled: true,
     config: {
-      playgroundAlways: false,
-      defaultLimit: 10,
-      maxLimit: 20,
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
       apolloServer: {
-        tracing: true,
-      },
-    }
-  },
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
+        tracing: false,
+        introspection: true,
       },
     },
   },
-});
+};
